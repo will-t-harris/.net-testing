e37bbc582e00db468a23c10ba1064c37629988fe
Will Harris <will.t.harris@protonmail.com>
2019-10-29 18:53:06 -0600

    Initialize CupOrderAdmin tests project

---
+++ WireBrainCoffee.CupOrderAdmin.Core.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WireBrainCoffee.CupOrderAdmin.Core.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
