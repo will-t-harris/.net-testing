299b1a88a119cfce4655efbedbf86ff9fb4f04be
Will Harris <will.t.harris@protonmail.com>
2019-10-29 19:09:56 -0600

    update gitignore, create test file in example test directory

---
+++ WireBrainCoffee.CupOrderAdmin.Core.Tests/Services/OrderCreation/OrderCreationServiceTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WireBrainCoffee.CupOrderAdmin.Core.Tests.Services.OrderCreation
+{
+    [TestClass]
+
+    public class OrderCreationServiceTests
+    {
+        [TestMethod]
+        public void ShouldStoreCreatedOrderInOrderCreationResult()
+        {
+
+        }
+    }
+}
